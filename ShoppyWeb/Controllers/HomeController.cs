3e3ee9ea890cbcfccc7de12d50ca9c2f0c0ec634
josephW1805 <joseph.weng.1805@gmail.com>
2024-04-18 18:52:32 -0400

    firstcommit

--- ShoppyWeb/Controllers/HomeController.cs
+++ ShoppyWeb/Controllers/HomeController.cs
@@ -4,14 +4,9 @@ using ShoppyWeb.Models;
 
 namespace ShoppyWeb.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
 
     public IActionResult Index()
     {
