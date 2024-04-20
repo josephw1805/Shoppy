43b46bc753b79190b85a2d76a1fafc0eee0e96e1
josephW1805 <joseph.weng.1805@gmail.com>
2024-04-20 14:55:32 -0400

    CRUD in Category model

---
+++ ShoppyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppyWeb;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+  public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Category>().HasData(
+        new Category {Id=1, Name="Electronic Pop", DisplayOrder= 1},
+        new Category {Id=2, Name="Pop", DisplayOrder= 2},
+        new Category {Id=3, Name="Electronic, Rock, Pop", DisplayOrder= 3}
+      );
+    }
+}
---
+++ ShoppyWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppyWeb;
+
+public class Category
+{
+  public int Id { get; set; }
+  [Required]
+  [MaxLength(30)]
+  [DisplayName("Category Name")]
+  public string Name { get; set; }
+  [DisplayName("Display Order")]
+  [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+  public int DisplayOrder { get; set; }
+}
