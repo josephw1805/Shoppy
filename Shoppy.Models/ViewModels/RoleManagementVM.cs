9250210deaf3c9877b0b60bcce4df5d0a21df21f
josephW1805 <joseph.weng.1805@gmail.com>
2024-04-30 17:00:59 -0400

    Manage user

---
+++ Shoppy.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Shoppy.Models;
+
+public class RoleManagementVM
+{
+  public ApplicationUser ApplicationUser { get; set; }
+  public IEnumerable<SelectListItem> RoleList { get; set; }
+  public IEnumerable<SelectListItem> CompanyList { get; set; }
+}
--- ShoppyWeb/Areas/Admin/Controllers/UserController.cs
+++ ShoppyWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Shoppy.DataAccess;
 using Shoppy.Models;
 using Shoppy.Utility;
@@ -8,9 +11,10 @@ namespace ShoppyWeb;
 
 [Area("Admin")]
 [Authorize(Roles = SD.Role_Admin)]
-public class UserController(IUnitOfWork unitOfWork) : Controller
+public class UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager) : Controller
 {
-  private readonly IUnitOfWork _unitOfWork = unitOfWork;
+  private readonly ApplicationDbContext _db = db;
+  private readonly UserManager<IdentityUser> _userManager = userManager;
 
   public IActionResult Index()
   {
@@ -21,23 +25,86 @@ public class UserController(IUnitOfWork unitOfWork) : Controller
   [HttpGet]
   public IActionResult GetAll()
   {
-    List<ApplicationUser> objUserList = [.. _unitOfWork.ApplicationUser.GetAll()];
+    List<ApplicationUser> objUserList = [.. _db.ApplicationUsers.Include(u => u.Company)];
+
+    var userRoles = _db.UserRoles.ToList();
+    var roles = _db.Roles.ToList();
+
+    foreach (var user in objUserList)
+    {
+      var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+      user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+    }
+
     return Json(new { data = objUserList });
   }
 
-  [HttpDelete]
-  public IActionResult Delete(int id)
+  public IActionResult RoleManagement(string userId)
   {
-    var companyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
-    if (companyToBeDeleted == null)
+    string RoleId = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+    RoleManagementVM RoleVM = new()
     {
-      return Json(new { success = false, message = "Error while deleting" });
+      ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+      RoleList = _db.Roles.Select(i => new SelectListItem
+      {
+        Text = i.Name,
+        Value = i.Name
+      }),
+      CompanyList = _db.Companies.Select(i => new SelectListItem
+      {
+        Text = i.Name,
+        Value = i.Id.ToString()
+      })
+    };
+    RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+    return View(RoleVM);
+  }
+
+  [HttpPost]
+  public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+  {
+    string RoleId = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+    string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+
+    if (roleManagementVM.ApplicationUser.Role != oldRole)
+    {
+      // a role was updated
+      ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+      if (roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+      {
+        applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+      }
+      if (oldRole == SD.Role_Company)
+      {
+        applicationUser.CompanyId = null;
+      }
+      _db.SaveChanges();
+      _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+      _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+    }
+    return RedirectToAction("Index");
+  }
+
+  [HttpPost]
+  public IActionResult LockUnlock([FromBody] string id)
+  {
+    var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+    if (objFromDb == null)
+    {
+      return Json(new { success = false, message = "Error while Locking/Unlocking" });
     }
 
-    _unitOfWork.Company.Remove(companyToBeDeleted);
-    _unitOfWork.Save();
-
-    return Json(new { success = true, message = "Delete successful" });
+    if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+    {
+      // user is currently locked and we need to unlock them
+      objFromDb.LockoutEnd = DateTime.Now;
+    }
+    else
+    {
+      objFromDb.LockoutEnd = DateTime.Now.AddYears(100);
+    }
+    _db.SaveChanges();
+    return Json(new { success = true, message = "Operation Successful" });
   }
   #endregion
 }
